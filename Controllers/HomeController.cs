a14ebe49cc559c0883fced61d6665753890e626b
Gunel <gunelfh@code.edu.az>
2022-06-05 17:45:21 +0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MVCHome_Work.Models;
+
+namespace MVCHome_Work.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            List<Slider> sliders = new List<Slider>
+            {
+                new Slider
+                {
+                    Id=1,
+                    Image="1.jpg",
+                    Title="New Collection!",
+                    Subtitle="Top Selling))",
+                    Url="https://www.google.com"
+                } ,
+                new Slider
+                {
+                    Id=2,
+                    Image="2.jpg",
+                    Title="Top Collection m",
+                    Subtitle="tp slng",
+                    Url="https://www.youtube.com"
+                }
+            };
+
+            return View(sliders);
+        }
+         public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Blog_detail()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+         public IActionResult ProductDetail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Slider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MVCHome_Work.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+
+        public string Image { get; set; }
+
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string Url { get; set; }
+    }
+}
